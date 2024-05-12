bbe07fa4e807f6d00f32757d885be21783640928
annyamd <miann9920@gmail.com>
2024-05-12 18:15:52 +0300

    Add models for pokemon details

--- app/src/main/java/org/itmo/pokemonapp/data/model/domain/Pokemon.kt
+++ app/src/main/java/org/itmo/pokemonapp/data/model/domain/Pokemon.kt
@@ -1,6 +1,18 @@
 package org.itmo.pokemonapp.data.model.domain
 
+
 data class Pokemon(
     val id: Int,
-    val name: String
+    val name: String,
+    val height: Int,
+    val weight: Int,
+    val speciesName: String,
+    val types: List<String>,
+    val abilities: List<String>,
+    val stats: List<Stat>
+)
+
+data class Stat(
+    val name: String,
+    val value: Int
 )
--- app/src/main/java/org/itmo/pokemonapp/data/model/dto/PokemonDto.kt
+++ app/src/main/java/org/itmo/pokemonapp/data/model/dto/PokemonDto.kt
@@ -1,6 +1,26 @@
 package org.itmo.pokemonapp.data.model.dto
 
+import com.google.gson.annotations.SerializedName
+
 data class PokemonDto(
     val id: Int,
-    val name: String
+    val name: String,
+    val height: Int,
+    val weight: Int,
+    val species: NamedResource,
+    val types: List<TypeDto>,
+    val abilities: List<AbilityDto>,
+    val stats: List<StatDto>
+)
+
+data class TypeDto(val type: NamedResource)
+
+data class AbilityDto(val ability: NamedResource)
+
+data class NamedResource(val name: String, val url: String)
+
+data class StatDto(
+    val stat: NamedResource,
+    @SerializedName("base_stat")
+    val value: Int
 )
